0e69e45949f1a86eb6fe2a672b5314ab8cb6d9b7
Junaid Atique <junaid@rush.app>
2024-02-17 15:21:59 +0500

    adding qr pages

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -23,6 +23,7 @@ export default function App() {
           Home
         </Link>
         <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/settings">Settings page</Link>
       </ui-nav-menu>
       <Outlet />
     </AppProvider>
